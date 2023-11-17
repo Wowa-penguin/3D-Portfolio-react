56937da89b90ee71cebde0a143ae9f73ec1612bc
Heimir snær <heimir2710@gmail.com>
2023-11-17 13:02:37 +0000

    Hero komið næstum

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,37 @@
 import { BrowserRouter } from "react-router-dom";
 
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components/index";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components/index";
 
 const App = () => {
-  
   return (
     <BrowserRouter>
       <div className="relative z-0 bg-primary">
         <div className=" bg-hero-pattern bg-cover bg-no-repeat bg-center">
-          <Navbar /> 
-          <Hero /> 
+          <Navbar />
+          <Hero />
         </div>
-        <About /> 
-        <Experience /> 
-        <Tech /> 
-        <Works /> 
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
         <Feedbacks />
         <div className="relative z-0">
-          <Contact /> 
-          <StarsCanvas /> 
+          <Contact />
+          <StarsCanvas />
         </div>
-      </div>    
+      </div>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,47 +1,76 @@
-import * as THREE from 'three';
-import { Suspense } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
-const Computers = () => {
-  const computer = useGLTF('./desktop_pc/scene.gltf')
+const Computers = ({ isMobile }) => {
+  const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
-    <group>
-      <hemisphereLight intensity={[1]} groundColor="black" position={[0, 10, 0]} />
-      <pointLight intensity={[1]} position={[10, 10, 10]} />
+    <mesh>
+      <hemisphereLight
+        intensity={[2]}
+        groundColor="black"
+        position={[0, 10, 0]}
+      />
+      <pointLight intensity={[2]} position={[10, 10, 10]} />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
 
       <primitive
         object={computer.scene}
-        scale={0.75}
-        position={[0, -3.25, -1.5]}  
+        scale={isMobile ? 0.6 : 0.8}
+        position={isMobile ? [0, -5, -1.5] : [0, -5, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
-    </group>
-  )
-}
+    </mesh>
+  );
+};
 
 const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    setIsMobile(mediaQuery.matches);
+
+    const hendleMediaQueryChange = (event) => {
+      setIsMobile(mediaQuery.matches);
+    };
+
+    mediaQuery.addEventListener("change", hendleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", hendleMediaQueryChange);
+    };
+  }, []);
+
   return (
     <Canvas
-      frameloop='demand'
+      frameloop="demand"
       shadows
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls 
+        <OrbitControls
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 2} 
+          minPolarAngle={Math.PI / 2}
         />
-        <Computers /> 
+        <Computers isMobile={isMobile} />
       </Suspense>
       <Preload all />
     </Canvas>
-  )
-}
+  );
+};
 
 export default ComputersCanvas;
